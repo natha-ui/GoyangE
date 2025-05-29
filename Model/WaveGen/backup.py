f65e5baf4b0f51d3f40883989bab12cc697f97ea
natha-ui <136472669+natha-ui@users.noreply.github.com>
2025-05-29 13:30:20 +0900

    Update backup.py

--- Model/WaveGen/backup.py
+++ Model/WaveGen/backup.py
@@ -1,38 +1,44 @@
-from __future__ import print_function
+import glob
+import os
+import shutil
+import sys
+import time
+
+import tensorflow as tf
+
+def wait_for_first_checkpoint(train_dir):
+    while tf.train.latest_checkpoint(train_dir) is None:
+        print('Waiting for first checkpoint...')
+        time.sleep(1)
+
+def backup_checkpoints(train_dir, backup_dir, interval_sec):
+    while True:
+        latest_ckpt = tf.train.latest_checkpoint(train_dir)
+        time.sleep(2)  # Ensure all checkpoint files are flushed
+
+        for fp in glob.glob(latest_ckpt + '*'):
+            name = os.path.basename(fp)
+            backup_fp = os.path.join(backup_dir, name)
+            print(f'{fp} -> {backup_fp}')
+            shutil.copyfile(fp, backup_fp)
+
+        print('-' * 80)
+        time.sleep(interval_sec)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <train_dir> <n_minutes>")
+        sys.exit(1)
+
+    train_dir = sys.argv[1]
+    nmin = float(sys.argv[2])
+    nsec = int(nmin * 60)
+
+    backup_dir = os.path.join(train_dir, 'backup')
+    os.makedirs(backup_dir, exist_ok=True)
+
+    wait_for_first_checkpoint(train_dir)
+    backup_checkpoints(train_dir, backup_dir, nsec)
 
 if __name__ == '__main__':
-  import glob
-  import os
-  import shutil
-  import sys
-  import time
-
-  import tensorflow as tf
-
-  train_dir, nmin = sys.argv[1:3]
-  nsec = int(float(nmin) * 60.)
-
-  backup_dir = os.path.join(train_dir, 'backup')
-
-  if not os.path.exists(backup_dir):
-    os.makedirs(backup_dir)
-
-  while tf.train.latest_checkpoint(train_dir) is None:
-    print('Waiting for first checkpoint')
-    time.sleep(1)
-
-  while True:
-    latest_ckpt = tf.train.latest_checkpoint(train_dir)
-
-    # Sleep for two seconds in case file flushing
-    time.sleep(2)
-
-    for fp in glob.glob(latest_ckpt + '*'):
-      _, name = os.path.split(fp)
-      backup_fp = os.path.join(backup_dir, name)
-      print(f'{fp}->{backup_fp}')
-      shutil.copyfile(fp, backup_fp)
-    print('-' * 80)
-
-    # Sleep for the specified interval
-    time.sleep(nsec)
+    main()
