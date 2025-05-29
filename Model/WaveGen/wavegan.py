78198e7a58b9902683fdc96b2c699c72ed57cf93
natha-ui <136472669+natha-ui@users.noreply.github.com>
2025-05-29 13:31:58 +0900

    Update wavegan.py

--- Model/WaveGen/wavegan.py
+++ Model/WaveGen/wavegan.py
@@ -1,121 +1,112 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, Model
 
 
 def conv1d_transpose(inputs, filters, kernel_width, stride=4, padding='same', upsample='zeros'):
     if upsample == 'zeros':
-        x = tf.expand_dims(inputs, axis=1)
+        x = tf.expand_dims(inputs, axis=1)  # [B, 1, W, C]
         x = layers.Conv2DTranspose(filters, (1, kernel_width), strides=(1, stride), padding=padding)(x)
-        return x[:, 0]
+        return tf.squeeze(x, axis=1)
     elif upsample == 'nn':
-        _, w, nch = inputs.shape.as_list()
-        x = tf.expand_dims(inputs, axis=1)
-        x = tf.image.resize(x, [1, w * stride], method='nearest')
-        x = x[:, 0]
+        x = tf.expand_dims(inputs, axis=1)  # [B, 1, W, C]
+        x = tf.image.resize(x, [1, inputs.shape[1] * stride], method='nearest')
+        x = tf.squeeze(x, axis=1)
         return layers.Conv1D(filters, kernel_width, strides=1, padding=padding)(x)
     else:
-        raise NotImplementedError
+        raise NotImplementedError(f"Unsupported upsample mode: {upsample}")
 
 
-"""
-  Input: [None, 100]
-  Output: [None, slice_len, 1]
-"""
-def WaveGANGenerator(z, slice_len=16384, nch=1, kernel_len=25, dim=64, use_batchnorm=False, upsample='zeros', train=False):
-    assert slice_len in [16384, 32768, 65536]
+def WaveGANGenerator(z, slice_len=16384, nch=1, kernel_len=25, dim=64, use_batchnorm=False, upsample='zeros'):
+    assert slice_len in [16384, 32768, 65536], "Unsupported slice_len"
+
     dim_mul = 16 if slice_len == 16384 else 32
-
-    batchnorm = lambda x: layers.BatchNormalization()(x) if use_batchnorm else x
-
     x = layers.Dense(16 * dim * dim_mul)(z)
     x = tf.reshape(x, [-1, 16, dim * dim_mul])
-    x = batchnorm(x)
+    if use_batchnorm:
+        x = layers.BatchNormalization()(x)
     x = tf.nn.relu(x)
     dim_mul //= 2
 
-    for i in range(4):
+    for _ in range(4):
         x = conv1d_transpose(x, dim * dim_mul, kernel_len, 4, upsample=upsample)
-        x = batchnorm(x)
+        if use_batchnorm:
+            x = layers.BatchNormalization()(x)
         x = tf.nn.relu(x)
         dim_mul //= 2
 
     if slice_len == 16384:
         x = conv1d_transpose(x, nch, kernel_len, 4, upsample=upsample)
-        x = tf.nn.tanh(x)
     elif slice_len == 32768:
         x = conv1d_transpose(x, dim, kernel_len, 4, upsample=upsample)
-        x = batchnorm(x)
+        if use_batchnorm:
+            x = layers.BatchNormalization()(x)
         x = tf.nn.relu(x)
         x = conv1d_transpose(x, nch, kernel_len, 2, upsample=upsample)
-        x = tf.nn.tanh(x)
     elif slice_len == 65536:
         x = conv1d_transpose(x, dim, kernel_len, 4, upsample=upsample)
-        x = batchnorm(x)
+        if use_batchnorm:
+            x = layers.BatchNormalization()(x)
         x = tf.nn.relu(x)
         x = conv1d_transpose(x, nch, kernel_len, 4, upsample=upsample)
-        x = tf.nn.tanh(x)
 
-    return x
+    return tf.nn.tanh(x)
 
 
 def lrelu(inputs, alpha=0.2):
-  return tf.nn.leaky_relu(inputs, alpha=alpha)
+    return tf.nn.leaky_relu(inputs, alpha=alpha)
 
 
-def apply_phaseshuffle(x, rad, pad_type='reflect'):
-  b, x_len, nch = x.shape.as_list()
+def apply_phaseshuffle(x, rad, pad_type='REFLECT'):
+    input_shape = tf.shape(x)
+    b, x_len, nch = input_shape[0], input_shape[1], input_shape[2]
 
-  phase = tf.random_uniform([], -rad, rad + 1, dtype=tf.int32)
-  pad_l = tf.maximum(phase, 0)
-  pad_r = tf.maximum(-phase, 0)
-  phase_start = pad_r
-  x = tf.pad(x, [[0, 0], [pad_l, pad_r], [0, 0]], mode=pad_type)
-  x = x[:, phase_start:phase_start+x_len]
-  x.set_shape([b, x_len, nch])
-  return x
+    phase = tf.random.uniform([], -rad, rad + 1, dtype=tf.int32)
+    pad_l = tf.maximum(phase, 0)
+    pad_r = tf.maximum(-phase, 0)
+    phase_start = pad_r
+    x = tf.pad(x, [[0, 0], [pad_l, pad_r], [0, 0]], mode=pad_type)
+    x = x[:, phase_start:phase_start + x_len]
+    x.set_shape([None, x_len, nch])
+    return x
 
 
-"""
-  Input: [None, slice_len, nch]
-  Output: [None] (linear output)
-"""
 def WaveGANDiscriminator(x, kernel_len=25, dim=64, use_batchnorm=False, phaseshuffle_rad=0):
-    batchnorm = lambda x: layers.BatchNormalization()(x) if use_batchnorm else x
-    phaseshuffle = lambda x: apply_phaseshuffle(x, phaseshuffle_rad) if phaseshuffle_rad > 0 else x
+    def maybe_bn(y): return layers.BatchNormalization()(y) if use_batchnorm else y
+    def maybe_ps(y): return apply_phaseshuffle(y, phaseshuffle_rad) if phaseshuffle_rad > 0 else y
 
     x = layers.Conv1D(dim, kernel_len, strides=4, padding='same')(x)
     x = lrelu(x)
-    x = phaseshuffle(x)
+    x = maybe_ps(x)
 
     x = layers.Conv1D(dim * 2, kernel_len, strides=4, padding='same')(x)
-    x = batchnorm(x)
+    x = maybe_bn(x)
     x = lrelu(x)
-    x = phaseshuffle(x)
+    x = maybe_ps(x)
 
     x = layers.Conv1D(dim * 4, kernel_len, strides=4, padding='same')(x)
-    x = batchnorm(x)
+    x = maybe_bn(x)
     x = lrelu(x)
-    x = phaseshuffle(x)
+    x = maybe_ps(x)
 
     x = layers.Conv1D(dim * 8, kernel_len, strides=4, padding='same')(x)
-    x = batchnorm(x)
+    x = maybe_bn(x)
     x = lrelu(x)
-    x = phaseshuffle(x)
+    x = maybe_ps(x)
 
     x = layers.Conv1D(dim * 16, kernel_len, strides=4, padding='same')(x)
-    x = batchnorm(x)
+    x = maybe_bn(x)
     x = lrelu(x)
 
-    if x.shape[1] * 4 == 32768:
+    final_length = x.shape[1] * 4
+    if final_length == 32768:
         x = layers.Conv1D(dim * 32, kernel_len, strides=2, padding='same')(x)
-        x = batchnorm(x)
+        x = maybe_bn(x)
         x = lrelu(x)
-    elif x.shape[1] * 4 == 65536:
+    elif final_length == 65536:
         x = layers.Conv1D(dim * 32, kernel_len, strides=4, padding='same')(x)
-        x = batchnorm(x)
+        x = maybe_bn(x)
         x = lrelu(x)
 
     x = layers.Flatten()(x)
     x = layers.Dense(1)(x)
     return tf.squeeze(x, axis=1)
-
